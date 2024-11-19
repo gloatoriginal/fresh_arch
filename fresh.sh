0f7435b6499127ec51b1a98a139fa9990502b1ee
Devin Baughman <47373236+gloatoriginal@users.noreply.github.com>
2024-11-18 20:08:20 -0800

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -17,14 +17,23 @@ paru -S variety solaar \
         kexec-tools cronie \
         virt-manager moonlight-qt-bin youtube-music-bin stremio \
         filelight discover partitionmanager \
+        zsh-autosuggestions zsh-syntax-highlighting zsh \
         #nvidia-open-dkms \
         --noconfirm
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/' ~/.zshrc
 
 
 ## Setup kexec updater
 git clone https://github.com/gloatoriginal/arch_update \
         && chmod +x ./arch_update/bootstrap.sh \
         && ./arch_update/bootstrap.sh
+
 
 
 systemctl enable hhd@gloat.service
