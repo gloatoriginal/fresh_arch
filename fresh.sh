7e55f11fc6eb3a3bc5c72dcc1c789caea2ec60d7
Devin Baughman <47373236+gloatoriginal@users.noreply.github.com>
2024-11-18 20:43:47 -0800

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -3,6 +3,7 @@
 ## Get me paru and some AUR STAT
 sudo pacman -S --needed base-devel
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.bashrc
 git clone https://aur.archlinux.org/paru.git
 cd paru && makepkg -si
 
@@ -10,7 +11,7 @@ cd ~
 ## Install necessities
 paru -S variety solaar \
         flatpak flatseal \
-        steam hhd heroic-games-launcher-bin \
+        steam heroic-games-launcher-bin \
         visual-studio-code-bin dbeaver git \
         linux-lts linux-lts-headers \
         linux-rt linux-rt-headers \
@@ -21,14 +22,6 @@ paru -S variety solaar \
         #nvidia-open-dkms \
         --noconfirm
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/' ~/.zshrc
-
-
 ## Setup kexec updater
 git clone https://github.com/gloatoriginal/arch_update \
         && chmod +x ./arch_update/bootstrap.sh \
@@ -36,4 +29,10 @@ git clone https://github.com/gloatoriginal/arch_update \
 
 
 
-systemctl enable hhd@gloat.service
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/' ~/.zshrc
