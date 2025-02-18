f481ae6ab5d87af0a44ec26a91873b09344a5e89
Devin Baughman <47373236+gloatoriginal@users.noreply.github.com>
2025-02-17 19:47:48 -0800

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -17,7 +17,7 @@ paru -S variety solaar \
         flatpak flatseal \
         steam heroic-games-launcher-bin \
         visual-studio-code-bin proton-ge-custom-bin dbeaver git \
-        cronie firefox timeshift \
+        cronie firefox timeshift power-profiles-daemon \
         virt-manager moonlight-qt-bin youtube-music-bin stremio \
         filelight discover partitionmanager \
         zsh-autosuggestions zsh-syntax-highlighting zsh-fast-syntax-highlighting zsh \
