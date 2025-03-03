524555cc8573cb300cf4a8ddb43253575beddce1
Devin Baughman <47373236+gloatoriginal@users.noreply.github.com>
2025-03-02 16:58:35 -0800

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -5,8 +5,8 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-echo "[chaotic-aur]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+echo '[chaotic-aur]' | sudo tee -a /etc/pacman.conf
+echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
 
 sudo pacman -Syu --noconfirm
 
